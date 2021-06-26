ed49e4e8f08c4ac73537e498d1330ad0479ba1aa
Pieter <pieter@honoki.net>
2021-06-26 09:20:04 +0200

    Persist generated keys when restarting containers

--- docker-entrypoint.d/nginx-create-self-signed-cert.sh
+++ docker-entrypoint.d/nginx-create-self-signed-cert.sh
@@ -1,4 +1,7 @@
 # create a self-signed certificate for nginx reverse proxy
-mkdir -p /etc/nginx/
-openssl genrsa > /etc/nginx/privkey.pem
-openssl req -new -x509 -key /etc/nginx/privkey.pem -out /etc/nginx/cert.pem -days 1095 -subj "/C=BE/ST=BBRF/L=BBRF/O=BBRF/CN=bbrf-server"
+# but only if it doesn't exist yet
+if [ ! -f "/etc/nginx/keys/privkey.pem" ]; then
+    mkdir -p /etc/nginx/keys/
+    openssl genrsa > /etc/nginx/keys/privkey.pem
+    openssl req -new -x509 -key /etc/nginx/keys/privkey.pem -out /etc/nginx/keys/cert.pem -days 1095 -subj "/C=BE/ST=BBRF/L=BBRF/O=BBRF/CN=bbrf-server"
+fi
