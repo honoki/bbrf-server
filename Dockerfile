b46c3b9ea4925b91755a2be8197a3c36df1f07b2
Pieter <pieter@honoki.net>
2021-07-15 16:38:35 +0300

    restructuring + fixing data persistence misconfiguration

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM couchdb:3.1.1
 
-COPY views.json /tmp/
-COPY docker-entrypoint.sh /usr/local/bin/
-COPY bbrf-init.sh /usr/local/bin/
+COPY couchdb/views.json /tmp/
+COPY couchdb/docker-entrypoint.sh /usr/local/bin/
+COPY couchdb/bbrf-init.sh /usr/local/bin/
 
 RUN apt-get install -y curl
---
+++ couchdb/bbrf-init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sleep 10
+
+COUCHDB=http://bbrf-server_couchdb_1:5984/
+AUTH=$COUCHDB_USER:$COUCHDB_PASSWORD
+
+if [ -z "$BBRF_PASSWORD" ]; then
+    BBRF_PASSWORD=$(openssl rand -hex 32)
+    echo "[BBRF] Created following password for user bbrf: $BBRF_PASSWORD"
+fi
+
+# create the _users db
+curl -X PUT $COUCHDB"_users" -u $AUTH -s > /dev/null
+
+# create the bbrf user
+curl -X PUT -X PUT $COUCHDB"/_users/org.couchdb.user:bbrf" -u $AUTH \
+     -H "Accept: application/json" \
+     -H "Content-Type: application/json" \
+     -d '{"name": "bbrf", "password": "'$BBRF_PASSWORD'", "roles": [], "type": "user"}' -s > /dev/null
+
+# create a new database called bbf
+curl -X PUT $COUCHDB"bbrf" -u $AUTH -s > /dev/null
+
+# grant access rights to the new database
+curl -X PUT $COUCHDB"bbrf/_security" -u $AUTH -d "{\"admins\": {\"names\": [],\"roles\": []}, \"members\": {\"names\": [\"bbrf\"],\"roles\": []}}" -s > /dev/null
+
+# push bbrf views
+curl -X PUT $COUCHDB"bbrf/_design/bbrf" -u $AUTH -H "Content-Type: application/json" -d @/tmp/views.json -s > /dev/null
+
+# enable CORS
+curl -X PUT $COUCHDB"_node/_local/_config/httpd/enable_cors" -u $AUTH -d '"true"' -s > /dev/null
+
+# allow origin from dashboard on https://bbrf.me
+curl -X PUT $COUCHDB"_node/_local/_config/cors/origins" -u $AUTH -d '"https://bbrf.me"' -s > /dev/null
+curl -X PUT $COUCHDB"_node/_local/_config/cors/credentials" -u $AUTH -d '"true"' -s > /dev/null
+
+echo "[BBRF] Initialization complete"
