93ff29a77c48f4a7b8b66c6ddbd030d32ee90348
honoki <pieter@honoki.net>
2025-07-10 22:25:07 +0200

    don't need the proxy config

--- proxy/docker-entrypoint.d/nginx-create-self-signed-cert.sh
+++
@@ -1,7 +0,0 @@
-# create a self-signed certificate for nginx reverse proxy
-# but only if it doesn't exist yet
-if [ ! -f "/etc/nginx/keys/privkey.pem" ]; then
-    mkdir -p /etc/nginx/keys/
-    openssl genrsa > /etc/nginx/keys/privkey.pem
-    openssl req -new -x509 -key /etc/nginx/keys/privkey.pem -out /etc/nginx/keys/fullchain.pem -days 1095 -subj "/C=BE/ST=BBRF/L=BBRF/O=BBRF/CN=bbrf-server"
-fi
